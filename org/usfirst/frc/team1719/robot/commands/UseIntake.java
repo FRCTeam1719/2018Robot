996cb7bd107dd3fdbc618624545d3ad4513f1d76
a-bad-programmer <quintinf19@parkschool.net>
2018-01-15 11:29:44 -0500

    added overrides

--- org/usfirst/frc/team1719/robot/commands/UseIntake.java
+++ org/usfirst/frc/team1719/robot/commands/UseIntake.java
@@ -25,17 +25,22 @@ public class UseIntake extends Command {
         // eg. requires(chassis);
         intake = _intake;
         intakeSpeed = _intakeSpeed;
+        requires(intake);
+        
     }
     
     // Called just before this Command runs the first time
+    @Override
     protected void initialize() {}
     
     // Called repeatedly when this Command is scheduled to run
+    @Override
     protected void execute() {
         intake.set(intakeSpeed);
     }
     
     // Make this return true when this Command no longer needs to run execute()
+    @Override
     protected boolean isFinished() {
         return false;
     }
