0e5a32e0e503d4457ce06eaf42a089fbb9eeed63
gusg21 <gusg21@parkschool.net>
2018-02-10 15:18:46 -0500

    Some wrist javadocs for the put

--- org/usfirst/frc/team1719/robot/subsystems/Wrist.java
+++ org/usfirst/frc/team1719/robot/subsystems/Wrist.java
@@ -4,6 +4,9 @@ import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 /**
+ * Simple Wrist subsystem for the robot.
+ * 
+ * @author gusg21
  *
  */
 public class Wrist extends Subsystem {
@@ -16,11 +19,29 @@ public class Wrist extends Subsystem {
 
     public void initDefaultCommand() {}
     
+    /**
+     * Put the wrist up.
+     * 
+     */
     public void putUp() {
         wristSolenoid.set(false);
     }
     
+    /**
+     * Put the wrist down.
+     * 
+     */
+    
     public void putDown() {
         wristSolenoid.set(true);
+    }
+    
+    /**
+     * Set the state of the wrist
+     * 
+     * @param wrist
+     */
+    public void put(boolean wrist) {
+        wristSolenoid.set(wrist);
     }
 }
